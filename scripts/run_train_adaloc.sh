83047ea44b390dce791a4feb3289f6eb3f9694cd
Manyi <49282575+manyifire@users.noreply.github.com>
2025-07-17 00:00:45 +0800

    Update run_train_adaloc.sh

--- scripts/run_train_adaloc.sh
+++ scripts/run_train_adaloc.sh
@@ -5,12 +5,20 @@ export PYTHONPATH=$PWD
 DATA_FOLDER=data/Text_Localization/Goodnews
 
 
+# python AdaLoc/train.py \
+#   --model_name=adaloc_goodnews\
+#   --train_file=${DATA_FOLDER}/goodnews_train-gpt2-xl-art10000-seg2.json\
+#   --test_file=${DATA_FOLDER}/goodnews_test-gpt2-xl-art1000-seg3.json\
+#   --roberta_detector_name=roberta-large-openai-detector\
+#   --num_epoch=3\
+#   --batch_size=16
+
 python AdaLoc/train.py \
-  --model_name=adaloc_goodnews\
-  --train_file=${DATA_FOLDER}/goodnews_train-gpt2-xl-art10000-seg2.json\
-  --test_file=${DATA_FOLDER}/goodnews_val-gpt2-xl-art1000-seg3.json\
+  --model_name=mixed_code_mixed\
+  --train_file=code_train_mixed.json\
+  --test_file=code_test_mixed.json\
   --roberta_detector_name=roberta-large-openai-detector\
-  --num_epoch=3\
+  --num_epoch=2\
   --batch_size=16
 
 
